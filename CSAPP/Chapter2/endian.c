efceeedab698c350286ab1c2691085fb7294b01e
drov1x <yootme@hotmail.com>
2026-07-23 10:38:49 +0800

    更新D2L

--- CSAPP/Chapter2/endian.c
+++ CSAPP/Chapter2/endian.c
@@ -2,7 +2,7 @@
 
 int main(){
     int x = 0x01234567;
-    char *p = (char *)&x;
+    unsigned char *p = (char *)&x;
     printf("%02x %02x %02x %02x\n", (char)*p, (char)*(p+1), (char)*(p+2), (char)*(p+3));
     //67开头为小端，01开头为大端
     return 0;
